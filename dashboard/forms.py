abd85f847322deb1a9b3dd6518e77d2d675acc53
jslucassf <jslucassf@gmail.com>
2015-06-29 14:56:35 -0400

    Fix import error

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 #from django.contrib.auth.models import User
 from dashboard.models import Classroom
-from models import User
-from django.utils.translation import ugettext_lazy as _
+from dashboard.models import User
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
